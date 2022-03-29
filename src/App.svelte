ee51b0df134ebac22c5420d5ca2c07472631e8ac
Arebos <lent1510@gmail.com>
2022-03-29 11:06:08 +0900

    first-commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import TailwindCss from './TailwindCSS.svelte';
+
+	const fragment = new URLSearchParams(window.location.hash.slice(1));
+	const [accessToken, tokenType] = [
+		fragment.get('access_token'),
+		fragment.get('token_type'),
+	];
+	console.log(accessToken, tokenType);
+</script>
+
+<TailwindCss />
+<main class="container">
+	<p>test</p>
+	<div />
+</main>
+
+<style>
+</style>
